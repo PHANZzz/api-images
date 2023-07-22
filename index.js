756e021731a88da121926c002a964536d3270bcc
Kan Sophan <ytbro65@gmail.com>
2023-07-22 18:47:09 +0700

    first

--- index.js
+++ index.js
@@ -1,81 +1,95 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
 const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
-// Replace with your MongoDB connection string
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+// Use the provided uri string
 const uri = "mongodb+srv://sophan:sophan%40123@cluster0.r3agzsk.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+// Connect to the MongoDB Atlas database
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a schema for the image data
+const imageSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  img: {
+    data: Buffer,
+    contentType: String
   }
 });
-app.use((req, res, next) => {
-  res.removeHeader('Content-Security-Policy');
-  next();
+
+// Create a model for the image data
+const Image = mongoose.model('Image', imageSchema);
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Upload Image</h1>
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="text" name="name" placeholder="Name">
+      <input type="text" name="desc" placeholder="Description">
+      <input type="file" name="image">
+      <button type="submit">Upload</button>
+    </form>
+  `);
 });
 
 app.post('/upload', upload.single('image'), async (req, res) => {
+  // Check if a file was uploaded
+  if (!req.file) {
+    res.status(400).send('No file uploaded');
+    return;
+  }
+
+  // Create an object with the image data
+  const obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(path.join(__dirname, 'uploads', req.file.filename)),
+      contentType: 'image/png'
+    }
+  };
+
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // Specify the database and collection
+    const db = mongoose.connection.useDb('Stock');
+    const users = db.collection('Users');
 
-    const db = client.db('Stock');
-    const usersCollection = db.collection('Users');
+    // Insert the image data into the database
+    await users.insertOne(obj);
 
-    // Get the file data from the request
-    const fileData = req.file.buffer;
-
-    // Insert the image into the database
-    await usersCollection.insertOne({
-      image: fileData
-    });
-
-    // Get a list of uploaded images
-    const images = await usersCollection.find().toArray();
-
-    res.json(images);
+    // Send a success message to the browser
+    res.send('<p>Image uploaded successfully</p>');
   } catch (err) {
-    console.error(err);
-    res.status(500).send('An error occurred while uploading the image');
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log(err);
   }
 });
-
 app.get('/images', async (req, res) => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // Specify the database and collection
+    const db = mongoose.connection.useDb('Stock');
+    const users = db.collection('Users');
 
-    const db = client.db('Stock');
-    const usersCollection = db.collection('Users');
+    // Find all images in the database
+    const images = await users.find().toArray();
 
-    // Get a list of uploaded images
-    const images = await usersCollection.find().toArray();
-
-    res.json(images);
+    // Generate an HTML response with the image data
+    let html = '<h1>Images</h1>';
+    for (const image of images) {
+      html += `
+        <h2>${image.name}</h2>
+        <p>${image.desc}</p>
+        <img src="data:${image.img.contentType};base64,${image.img.data.toString('base64')}">
+      `;
+    }
+    res.send(html);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('An error occurred while retrieving the list of images');
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log(err);
   }
 });
 
