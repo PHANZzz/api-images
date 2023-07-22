3955821e783e30c2a7e4633a7715121c59249289
Kan Sophan <ytbro65@gmail.com>
2023-07-22 20:40:46 +0700

    first

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
+app.use(express.json());
+
 // Use the provided uri string
 const uri = "mongodb+srv://sophan:sophan%40123@cluster0.r3agzsk.mongodb.net/?retryWrites=true&w=majority";
 
@@ -26,19 +28,7 @@ const imageSchema = new mongoose.Schema({
 // Create a model for the image data
 const Image = mongoose.model('Image', imageSchema);
 
-app.get('/Insert', (req, res) => {
-  res.send(`
-    <h1>Upload Image</h1>
-    <form action="/upload" method="post" enctype="multipart/form-data">
-      <input type="text" name="name" placeholder="Name">
-      <input type="text" name="desc" placeholder="Description">
-      <input type="file" name="image">
-      <button type="submit">Upload</button>
-    </form>
-  `);
-});
-
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post('/api/images', upload.single('image'), async (req, res) => {
   // Check if a file was uploaded
   if (!req.file) {
     res.status(400).send('No file uploaded');
@@ -64,12 +54,13 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     await users.insertOne(obj);
 
     // Send a success message to the browser
-    res.send('<p>Image uploaded successfully</p>');
+    res.send({ message: 'Image uploaded successfully' });
   } catch (err) {
     console.log(err);
   }
 });
-app.get('/', async (req, res) => {
+
+app.get('/api/images', async (req, res) => {
   try {
     // Specify the database and collection
     const db = mongoose.connection.useDb('Stock');
@@ -78,16 +69,8 @@ app.get('/', async (req, res) => {
     // Find all images in the database
     const images = await users.find().toArray();
 
-    // Generate an HTML response with the image data
-    let html = '<h1>Images</h1>';
-    for (const image of images) {
-      html += `
-        <h2>${image.name}</h2>
-        <p>${image.desc}</p>
-        <img src="data:${image.img.contentType};base64,${image.img.data.toString('base64')}">
-      `;
-    }
-    res.send(html);
+    // Send the image data as a JSON response
+    res.send(images);
   } catch (err) {
     console.log(err);
   }
