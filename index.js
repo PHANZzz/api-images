b4c2140997faf1cbf9d11580fe9d48be404d2246
Kan Sophan <ytbro65@gmail.com>
2023-07-26 23:22:39 +0700

    last

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ const Image = mongoose.model('Image', imageSchema);
 app.get('/', (req, res) => {
   res.send(`
     <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     </head>
     <body class="bg-gray-100">
